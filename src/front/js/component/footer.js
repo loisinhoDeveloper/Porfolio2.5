2d7327652bcd87f30559fd577f03914883f09caa
Loisinho <159461422+loisinhoDeveloper@users.noreply.github.com>
2024-12-13 20:05:09 +0000

    retoques

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "../../styles/footer.css"; // Importamos los estilos específicos
+import Rodilla from "../../../front/img/rodillaLogo.png";
 
 export const Footer = () => { 
     return (
         <footer className="footer">
-            <p>© 2024 rodilla<span>.Dev</span> "Portafolio 2.5"</p>
+            <img src={Rodilla} alt="Rodilla Dev Logo" className="footer-logo" />
+            <p> © 2025 "Portafolio 2.5"</p>
+            
         </footer>
     );
 };
